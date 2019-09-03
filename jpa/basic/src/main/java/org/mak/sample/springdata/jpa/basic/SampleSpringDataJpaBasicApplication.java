7cc2c31fa86ac75041300913600b81d1dc8ab42f
M_Khodabandelu <m8a8h8d8i@gmail.com>
2019-09-03 15:00:12 +0430

    intial commit

---
+++ jpa/basic/src/main/java/org/mak/sample/springdata/jpa/basic/SampleSpringDataJpaBasicApplication.java
@@ -0,0 +1,13 @@
+package org.mak.sample.springdata.jpa.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringDataJpaBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringDataJpaBasicApplication.class, args);
+	}
+
+}
---
+++ jpa/basic/src/main/java/org/mak/sample/springdata/jpa/basic/security/domain/User.java
@@ -0,0 +1,25 @@
+package org.mak.sample.springdata.jpa.basic.security.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "core_user")
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(unique = true, length = 50, nullable = false)
+    private String username;
+
+    @Column(name = "password_hash", length = 60, nullable = false)
+    private String password;
+
+}
